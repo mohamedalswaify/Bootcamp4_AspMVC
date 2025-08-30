b6bf3aa28eef835fed73cba43cae41496abf3f1c
mohamedalswaify <mohamedgamal1455@gmail.com>
2025-08-30 21:55:53 +0300

    Add project files.

---
+++ Bootcamp4_AspMVC/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Bootcamp4_AspMVC.Data;
+using Bootcamp4_AspMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp4_AspMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        { 
+        IEnumerable<Category> categories= _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ Bootcamp4_AspMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Bootcamp4_AspMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bootcamp4_AspMVC.Data
+{
+
+
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+     
+        
+
+
+
+
+    }
+
+
+
+
+
+
+}
