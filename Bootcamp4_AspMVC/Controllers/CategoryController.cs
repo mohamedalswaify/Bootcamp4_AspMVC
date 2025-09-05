992f56d19aa92d7ca89c3ae31fd85314dfc329b8
mohamedalswaify <mohamedgamal1455@gmail.com>
2025-09-05 21:36:40 +0300

    category update and delet

--- Bootcamp4_AspMVC/Controllers/CategoryController.cs
+++ Bootcamp4_AspMVC/Controllers/CategoryController.cs
@@ -13,10 +13,129 @@ namespace Bootcamp4_AspMVC.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
-        { 
-        IEnumerable<Category> categories= _context.Categories.ToList();
-            return View(categories);
+        {
+            try {
+
+                IEnumerable<Category> categories = _context.Categories.ToList();
+                return View(categories);
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return Content("حدث خطا  غير متوقع يرجي مراجهة الدعم الفني:0565455252545");
+            }
         }
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+           try
+            {
+                if (!ModelState.IsValid)
+                {
+
+                    return View(category);
+
+                }
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex) {
+                return Content("حدث خطا  غير متوقع يرجي مراجهة الدعم الفني:0565455252545");
+            }
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var category = _context.Categories.Find(Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+
+                    return View(category);
+
+                }
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                return Content("حدث خطا  غير متوقع يرجي مراجهة الدعم الفني:0565455252545");
+            }
+        }
+
+
+
+
+
+
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var category = _context.Categories.Find(Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            try
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                return Content("حدث خطا  غير متوقع يرجي مراجهة الدعم الفني:0565455252545");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ Bootcamp4_AspMVC/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Bootcamp4_AspMVC.Data;
+using Bootcamp4_AspMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp4_AspMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> Products = _context.Products.ToList();
+            return View(Products);
+        }
+    }
+}
